0c3e938cc2d4a131704667fed8b37e4fb65c9690
Johann Oberleitner <johannoberleitner@Johanns-MacBook-Pro-16.local>
2024-07-10 01:16:49 +0200

    Added tests for Lexer

--- src/Parser.Tests/LexerTests.cs
+++ src/Parser.Tests/LexerTests.cs
@@ -11,8 +11,12 @@ public class Tests
     {
     }
 
+    private static IEnumerable<string> ConvertLexemsToPrintableStrings(Lexer lexer) 
+    =>  from lexem in lexer.Lexems
+                             select lexem.ToString();
+
     [Test]
-    public void Test1()
+    public void Test_SQLStatement()
     {
         var lexer = new Lexer("SELECT * FROM MyTable");
         lexer.Scan();
@@ -24,5 +28,45 @@ public class Tests
 
         Assert.That(tokensFromResult, Is.EqualTo(new List<string>(["SELECT", "*", "FROM", "MyTable", "END"])).AsCollection  );
         Assert.That(lexemTypesFromResult, Is.EqualTo(new List<Type>([typeof(TSqlKeyword), typeof(OperatorSymbol), typeof(TSqlKeyword), typeof(LexerIdentifier), typeof(EndSymbol)])).AsCollection  );
+    }
+
+    [Test]
+    public void Test_SQLIdentifier()
+    {
+        var lexer = new Lexer("SELECT dbo.myschema.anotherid FROM Something");
+        lexer.Scan();
+        Assert.That(ConvertLexemsToPrintableStrings(lexer), 
+            Is.EqualTo(new List<string>(["Keyword SELECT", "Id dbo", ".", "Id myschema", ".", "Id anotherid", "Keyword FROM", "Id Something", "END"])).AsCollection  );
+    }
+
+    [Test]
+    public void Test_LineComment_1()
+    {
+        var lexer = new Lexer("SELECT 1 -- this is a comment");
+        lexer.Scan();
+        var lexems = lexer.Lexems;
+        var readableLexems = from lexem in lexems
+                             select lexem.ToString();
+        Assert.That(readableLexems, Is.EqualTo(new List<string>(["Keyword SELECT", "INTEGER 1", "END"])).AsCollection  );
+    }
+
+    [Test]
+    public void Test_LineComment_2()
+    {
+        var lexer = new Lexer("-- SELECT 1 \n SELECT 2");
+        lexer.Scan();
+        var lexems = lexer.Lexems;
+        var readableLexems = from lexem in lexems
+                             select lexem.ToString();
+        Assert.That(readableLexems, Is.EqualTo(new List<string>(["Keyword SELECT", "INTEGER 2", "END"])).AsCollection  );
+    }
+
+    [Test]
+    public void Test_BlockComment_1()
+    {
+        var lexer = new Lexer("SELECT 1 /* this is a comment */, 2");
+        lexer.Scan();
+        Assert.That(ConvertLexemsToPrintableStrings(lexer), 
+            Is.EqualTo(new List<string>(["Keyword SELECT", "INTEGER 1", "SEPARATOR", "INTEGER 2", "END"])).AsCollection  );
     }
 }
